e28f1a1d656e3c34e75000d229341b2bb58900f2
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-01-12 20:47:44 +0530

    initial commit

--- src/Pages/EquipmentDetailPage.tsx
+++ src/Pages/EquipmentDetailPage.tsx
@@ -1,15 +1,123 @@
-import React from "react"
-import AccountDetails from "../Component/AccountDetail"
-import Sidebar from "../Component/Sidebar"
-function EquipmentDetailPage(){
-  return(
-    <body>
-        <AccountDetails/>
-        <div className="dashboard-container">
-            <Sidebar/>
-        </div>
-    </body>
-  )
+import React from "react";
+import AccountDetails from "../Component/AccountDetail";
+import Sidebar from "../Component/Sidebar";
 
+function EquipmentDetailPage() {
+  return (
+    <div>
+      {/* Account Details */}
+      <AccountDetails />
+
+      {/* Main Dashboard Container */}
+      <div className="dashboard-container">
+        <Sidebar />
+
+        <main className="content">
+          <h2 className="text-center anim1">Equipment Input</h2>
+          <br />
+
+          {/* Equipment Input Form */}
+          <div className="card anim1">
+            <div className="card-body mainContent">
+              <div className="card-body">
+                {/* Equipment Code */}
+                <div>
+                  Equipment Code: <label id="lbl4"> </label>
+                </div>
+                <br />
+
+                {/* Equipment Name */}
+                <div className="input-group mb-4">
+                  <span className="input-group-text" id="addon-wrapping">
+                    Equipment
+                  </span>
+                  <input
+                    id="equip_E"
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter equipment name"
+                    aria-label="Equipment Name"
+                    aria-describedby="addon-wrapping"
+                  />
+                </div>
+
+                {/* Equipment Type */}
+                <div className="input-group mb-3">
+                  <label className="input-group-text" htmlFor="type_E">
+                    Type
+                  </label>
+                  <select className="form-select" id="type_E">
+                    <option value="" disabled selected>
+                      Choose...
+                    </option>
+                    {/* Add dynamic options here */}
+                  </select>
+                </div>
+
+                {/* Equipment Status */}
+                <div className="input-group mb-3">
+                  <label className="input-group-text" htmlFor="status_E">
+                    Status
+                  </label>
+                  <select className="form-select" id="status_E">
+                    <option value="" disabled selected>
+                      Choose...
+                    </option>
+                    {/* Add dynamic options here */}
+                  </select>
+                </div>
+              </div>
+
+              {/* Action Buttons */}
+              <div>
+                <button type="button" className="btn btn-primary" id="addBtn_E">
+                  ADD
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  id="updateBtn_E"
+                >
+                  UPDATE
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-success delete"
+                  id="deleteBtn_E"
+                >
+                  DELETE
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  id="resetBtn_E"
+                >
+                  RESET
+                </button>
+              </div>
+            </div>
+
+            {/* Equipment Table */}
+            <div className="card-body mainContent">
+              <table className="table table-dark" id="equipmentTable">
+                <thead>
+                  <tr>
+                    <th scope="col">Equipment Code</th>
+                    <th scope="col">Equipment Name</th>
+                    <th scope="col">Equipment Type</th>
+                    <th scope="col">Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* Dynamic table rows will be added here */}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
-export default EquipmentDetailPage 
+
+export default EquipmentDetailPage;
--- src/Pages/EquipmentPage.tsx
+++ src/Pages/EquipmentPage.tsx
@@ -1,15 +1,151 @@
-import React from "react"
-import AccountDetails from "../Component/AccountDetail"
-import Sidebar from "../Component/Sidebar"
-function EquipmentPage(){
-  return(
-    <body>
-        <AccountDetails/>
-        <div className="dashboard-container">
-            <Sidebar/>
-        </div>
-    </body>
-  )
+import React from "react";
+import AccountDetails from "../Component/AccountDetail";
+import Sidebar from "../Component/Sidebar";
 
+function EquipmentPage() {
+  return (
+    <div>
+      {/* Account Details */}
+      <AccountDetails />
+
+      {/* Main Dashboard Container */}
+      <div className="dashboard-container">
+        <Sidebar />
+
+        <main className="content">
+          <h2 className="text-center anim1">Equipment Detail</h2>
+          <br />
+
+          {/* Equipment Input Form */}
+          <div className="card anim1">
+            <div className="card-body mainContent">
+              <div className="card-body">
+                {/* Equipment Detail Receipt Number */}
+                <div>
+                  Equipment Detail Receipt Number: <label id="lbl6"> </label>
+                </div>
+                <br />
+
+                {/* Staff ID */}
+                <div className="input-group mb-3">
+                  <label className="input-group-text" htmlFor="staffID_ED">
+                    Staff ID
+                  </label>
+                  <select className="form-select" id="staffID_ED">
+                    <option value="" disabled selected>
+                      Choose...
+                    </option>
+                    {/* Add dynamic options here */}
+                  </select>
+                </div>
+                <br />
+
+                {/* Field ID */}
+                <div className="input-group mb-3">
+                  <label className="input-group-text" htmlFor="fieldID_ED">
+                    Field ID
+                  </label>
+                  <select className="form-select" id="fieldID_ED">
+                    <option value="" disabled selected>
+                      Choose...
+                    </option>
+                    {/* Add dynamic options here */}
+                  </select>
+                </div>
+                <br />
+
+                {/* Equip ID */}
+                <div className="input-group mb-3">
+                  <label className="input-group-text" htmlFor="equipID_ED">
+                    Equip ID
+                  </label>
+                  <select className="form-select" id="equipID_ED">
+                    <option value="" disabled selected>
+                      Choose...
+                    </option>
+                    {/* Add dynamic options here */}
+                  </select>
+                </div>
+                <br />
+
+                {/* Log Date */}
+                <div className="form-group">
+                  <label htmlFor="logDate_ED">Date</label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="logDate_ED"
+                    name="dateInput"
+                  />
+                </div>
+                <br />
+
+                {/* Reason for Borrowing */}
+                <div className="mb-3">
+                  <label htmlFor="reason_Dis" className="form-label">
+                    Reason
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="reason_Dis"
+                    rows="3"
+                  ></textarea>
+                </div>
+                <br />
+
+                {/* Action Buttons */}
+                <div>
+                  <button type="button" className="btn btn-primary" id="addBtn_ED">
+                    ADD
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    id="updateBtn_ED"
+                  >
+                    UPDATE
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-success delete"
+                    id="deleteBtn_ED"
+                  >
+                    DELETE
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    id="resetBtn_ED"
+                  >
+                    RESET
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Equipment Table */}
+            <div className="card-body mainContent">
+              <table className="table table-dark" id="vehicleTable">
+                <thead>
+                  <tr>
+                    <th scope="col">EquipmentDetail Receipt Number</th>
+                    <th scope="col">Staff ID</th>
+                    <th scope="col">Field ID</th>
+                    <th scope="col">Equip ID</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Reason for Borrowing</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* Dynamic table rows will be added here */}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
-export default EquipmentPage 
+
+export default EquipmentPage;
